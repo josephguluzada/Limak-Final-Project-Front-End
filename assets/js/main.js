61cadf62d1f083054997243109b8cdbfff4bce84
yusifquluzada <yquluzade98@gmail.com>
2021-11-20 19:23:23 +0400

    User Panel Content Created and Username Button fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,23 +2,56 @@
 
 const accordion = document.getElementsByClassName("content-box");
 
+function removeActivesInAccordion() {
+    for (let i = 0; i < accordion.length; i++) {
+        accordion[i].classList.remove("active");
+    }
+}
+
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function () {
-        accordion[i].classList.toggle("active")
+        removeActivesInAccordion();
+        accordion[i].classList.toggle("active");
     })
 
 }
+
+
 
 // User Panel Button
 
 const userBtn = document.querySelector(".user-panel-btn");
 const dropDownContent = document.querySelector(".dropdown-menu");
 
-userBtn.addEventListener("click", () => {
-    dropDownContent.classList.toggle("active");
+userBtn.addEventListener("click", (e) => {
+    if (e.target.id === "userPanelBtn")
+        dropDownContent.classList.toggle("active");
+})
+
+document.addEventListener("mouseup", (e) => {
+    if (e.target.id !== "userPanelBtn" && e.target.id !== "userPanelList")
+        dropDownContent.classList.remove("active");
 })
 
 
+// User Panel Content Header
+
+const flagBtn = document.querySelectorAll("#headerList .nav-item .nav-link");
+
+function removeActivesInFlag() {
+    for (let i = 0; i < flagBtn.length; i++) {
+        flagBtn[i].classList.remove("active");
+    }
+}
+
+for (let i = 0; i < flagBtn.length; i++) {
+    flagBtn[i].addEventListener("click", function () {
+        if (!flagBtn[i].classList.contains("active")) {
+            removeActivesInFlag();
+            flagBtn[i].classList.add("active")
+        }
+    })
+}
 
 
 
