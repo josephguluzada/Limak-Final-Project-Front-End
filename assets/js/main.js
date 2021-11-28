a00b16b98b5ce2e8d972d78fc1be4f7558a40c15
yusifquluzada <yquluzade98@gmail.com>
2021-11-28 12:07:06 +0400

    Register Page Created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+
+
+
 // Accordion
 
 const accordion = document.getElementsByClassName("content-box");
@@ -163,22 +166,8 @@ for (let i = 0; i < checkLabelTwo.length; i++) {
     })
 }
 
-// Visibility of User Panel Password
-
-const eyeBtn = document.getElementById("eyeBtn");
-const eyeIcon = document.querySelector("#eyeBtn i");
-const passwordInput = document.getElementById("currentPass");
 
 
 
-eyeBtn.addEventListener("click", function () {
-    if (eyeIcon.classList.contains("fa-eye-slash")) {
-        eyeIcon.classList.remove("fa-eye-slash");
-        eyeIcon.classList.add("fa-eye");
-        passwordInput.type = "text"
-    } else {
-        eyeIcon.classList.remove("fa-eye");
-        eyeIcon.classList.add("fa-eye-slash");
-        passwordInput.type = "password"
-    }
-})
+
+
